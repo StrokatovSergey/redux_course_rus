fbf69013a8deed578ef46f2f83f7d81f0d044f1c
sergey strokatov <s.strokatov@mail.ru>
2021-06-24 18:22:51 +0300

    delete some trash

--- source/bus/ui/__tests__/reducer.js
+++ source/bus/ui/__tests__/reducer.js
@@ -6,23 +6,6 @@ const initialState = Map({
   isFetching: false,
   isOnline: false
 });
-//
-// export const uiReducer = (state = initialState, action) => {
-// 	switch (action.type) {
-// 		case types.START_FETCHING:
-// 			return state.set('isFetching', true);
-//
-// 		case types.STOP_FETCHING:
-// 			return state.set('isFetching', false );
-//
-// 		case types.SET_ONLINE_STATE:
-// 			return state.set('isOnline', true );
-//
-// 		case types.SET_OFFLINE_STATE:
-// 			return state.set('isOnline', false );
-//
-// 	}
-// }
 
 describe("ui reducer test", () => {
   it("should return default state", function() {
