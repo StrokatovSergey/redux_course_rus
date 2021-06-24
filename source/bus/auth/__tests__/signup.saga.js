a18a9c907dc348012ded44e9374f4f8287106343
sergey strokatov <s.strokatov@mail.ru>
2021-06-24 18:22:35 +0300

    add tests for signup.saga.js

---
+++ source/bus/auth/__tests__/signup.saga.js
@@ -0,0 +1,92 @@
+import { put, apply } from "redux-saga/effects";
+import { cloneableGenerator } from "redux-saga/utils";
+
+import { api } from "../../../REST";
+import { authActions } from "../actions";
+import { uiActions } from "../../ui/actions";
+import { profileActions } from "../../profile/actions";
+import { signup } from "../saga/workers/signup";
+
+const signupAction = authActions.signupAsync(__.userProfile);
+
+const saga = cloneableGenerator(signup)(signupAction);
+let clone = null;
+
+describe("signup saga", () => {
+  describe("should fetch", () => {
+    it("should dispatch startFetching action", function() {
+      expect(saga.next().value).toEqual(put(uiActions.startFetching()));
+    });
+
+    it("should call a fetch request", function() {
+      expect(saga.next().value).toEqual(
+        apply(api, api.auth.signup, [__.userProfile])
+      );
+      clone = saga.clone();
+    });
+
+  });
+
+  describe("should success response", () => {
+    it("should get a Success request", function() {
+      expect(saga.next(__.fetchResponseSuccess).value).toEqual(
+        apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
+      );
+    });
+
+    it("should dispatch fillProfile action Snapshot", function() {
+      expect(saga.next(__.responseDataSuccess).value).toMatchInlineSnapshot(`
+Object {
+  "@@redux-saga/IO": true,
+  "PUT": Object {
+    "action": Object {
+      "payload": Object {
+        "avatar": "TEST_AVATAR",
+        "firstName": "Walter",
+        "id": "TEST_ID",
+        "lastName": "White",
+        "token": "TEST_TOKEN",
+      },
+      "type": "FILL_PROFILE",
+    },
+    "channel": null,
+  },
+}
+`);
+    });
+
+    it("should dispatch authenticate action", function() {
+      expect(saga.next().value).toEqual(put(authActions.authenticate()));
+    });
+
+    it('should stop fetching in success', function() {
+    	expect(saga.next().value).toEqual(put(uiActions.stopFetching()))
+    });
+
+    it('should finish saga  in success', function() {
+    	expect(saga.next().done).toEqual(true)
+    });
+  });
+
+  describe("should have 400 status  response", () => {
+    it("should return 400 status response", function() {
+      expect(clone.next(__.fetchResponseFail400).value).toEqual(
+        apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
+      );
+    });
+
+    it("should emit error action", function() {
+      expect(clone.next(__.responseDataFail).value).toEqual(
+        put(uiActions.emitError(__.error, "signup worker"))
+      );
+    });
+
+    it("should stop fetching in fail", function() {
+      expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
+    });
+
+    it("should finish saga in fail", function() {
+      expect(clone.next().done).toEqual(true);
+    });
+  });
+});
